72e39e8c42b52e6693675902c1203138a01e66be
Ngahu <jamaalaraheem@gmail.com>
2016-11-30 13:12:13 +0300

    mapping of the url patterns

--- mysite/Portfolio/views.py
+++ mysite/Portfolio/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django import template
-from django.template import loader
 
 def index(request):
-    template = loader.get_template('Portfolio/index.html')
-
-    return HttpResponse(template.render(request))
+    """incharge of returning the home page """
+    return HttpResponse("tesla")
